3b5ca25b8317596e2bf5f2b691f2896a5e0e5f7e
Alan Rezende <alan@turbinabiz.com.br>
2021-04-05 20:38:00 -0300

    Added ESLint + Prettier auto format

--- src/lib-components/tailpieces-sample.vue
+++ src/lib-components/tailpieces-sample.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 interface SampleData {
   counter: number;
@@ -10,50 +10,53 @@ interface SampleData {
   };
 }
 
-export default /*#__PURE__*/defineComponent({
-  name: 'TailpiecesSample', // vue component name
+export default /*#__PURE__*/ defineComponent({
+  name: "TailpiecesSample", // vue component name
   data(): SampleData {
     return {
       counter: 5,
       initCounter: 5,
       message: {
         action: null,
-        amount: null,
-      },
+        amount: null
+      }
     };
   },
   computed: {
     changedBy() {
       const { message } = this as SampleData;
-      if (!message.action) return 'initialized';
-      return `${message.action} ${message.amount || ''}`.trim();
-    },
+      if (!message.action) return "initialized";
+      return `${message.action} ${message.amount || ""}`.trim();
+    }
   },
   methods: {
     increment(arg: Event | number): void {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
+      const amount = typeof arg !== "number" ? 1 : arg;
       this.counter += amount;
-      this.message.action = 'incremented by';
+      this.message.action = "incremented by";
       this.message.amount = amount;
     },
     decrement(arg: Event | number): void {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
+      const amount = typeof arg !== "number" ? 1 : arg;
       this.counter -= amount;
-      this.message.action = 'decremented by';
+      this.message.action = "decremented by";
       this.message.amount = amount;
     },
     reset(): void {
       this.counter = this.initCounter;
-      this.message.action = 'reset';
+      this.message.action = "reset";
       this.message.amount = null;
-    },
-  },
+    }
+  }
 });
 </script>
 
 <template>
   <div class="tailpieces-sample">
-    <p>The counter was {{ changedBy }} to <b>{{ counter }}</b>.</p>
+    <p>
+      The counter was {{ changedBy }} to <b>{{ counter }}</b
+      >.
+    </p>
     <button @click="increment">
       Click +1
     </button>
@@ -73,16 +76,16 @@ export default /*#__PURE__*/defineComponent({
 </template>
 
 <style scoped>
-  .tailpieces-sample {
-    display: block;
-    width: 400px;
-    margin: 25px auto;
-    border: 1px solid #ccc;
-    background: #eaeaea;
-    text-align: center;
-    padding: 25px;
-  }
-  .tailpieces-sample p {
-    margin: 0 0 1em;
-  }
+.tailpieces-sample {
+  display: block;
+  width: 400px;
+  margin: 25px auto;
+  border: 1px solid #ccc;
+  background: #eaeaea;
+  text-align: center;
+  padding: 25px;
+}
+.tailpieces-sample p {
+  margin: 0 0 1em;
+}
 </style>
