8bdb422242e999ab0b8b022988d4c187d132ab33
Alan Rezende <alan@turbinabiz.com.br>
2021-04-05 20:25:01 -0300

    Init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { TailpiecesSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  TailpiecesSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <tailpieces-sample />
+  </div>
+</template>
---
+++ tailpieces.d.ts
@@ -0,0 +1,7 @@
+import { DefineComponent, Plugin } from 'vue';
+
+
+declare const Tailpieces: Exclude<Plugin['install'], undefined>;
+export default Tailpieces;
+
+export const TailpiecesSample: DefineComponent<{}, {}, any>;
