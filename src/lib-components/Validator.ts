8dae3ee574dd572e15ab35a18d4e7ae54247eecb
Alan Rezende <alan@turbinabiz.com.br>
2021-04-05 21:05:52 -0300

    Added initial Validator class

---
+++ src/lib-components/Validator.ts
@@ -0,0 +1,70 @@
+export class Validator {
+  validate(
+    value: string | number | boolean | null | undefined,
+    rules: string
+  ): string {
+    if (typeof value == "string") {
+      return this.validateString(value, rules);
+    }
+    return "";
+  }
+
+  validateString(value: string, rules: string): string {
+    const rulesArray = rules.split("|");
+    let message = "";
+    rulesArray.some(rule => {
+      /**
+       * Required
+       */
+      if (rule == "required") {
+        if (value == "") {
+          message = "Campo Obrigatório";
+          return true;
+        }
+      }
+      /**
+       * size
+       * size:3
+       */
+      if (rule.split(":")[0] == "size") {
+        if (typeof value == "string") {
+          if (value.length != parseInt(rule.split(":")[1])) {
+            message = `Este campo precisa ter ${rule.split(":")[1]} caracteres`;
+            return true;
+          }
+        }
+      }
+
+      /**
+       * min
+       * ex: min:3
+       */
+      if (rule.split(":")[0] == "min") {
+        if (typeof value == "string") {
+          if (value.length < parseInt(rule.split(":")[1])) {
+            message = `Este campo precisa ter ao menos ${
+              rule.split(":")[1]
+            } caracteres`;
+            return true;
+          }
+        }
+      }
+      /**
+       * max
+       * ex: max:3
+       */
+      if (rule.split(":")[0] == "max") {
+        if (typeof value == "string") {
+          if (value.length > parseInt(rule.split(":")[1])) {
+            message = `Este campo precisa ter no máximo ${
+              rule.split(":")[1]
+            } caracteres`;
+            return true;
+          }
+        }
+      }
+    });
+
+    return message;
+  }
+}
